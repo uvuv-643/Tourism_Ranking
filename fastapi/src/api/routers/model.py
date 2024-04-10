0c7a902cd0a33e218b45aef0988a23a2be4c3f6a
artem <uvuv.space@gmail.com>
2024-04-10 18:03:23 +0300

    little fixes with photo model, added text route

--- fastapi/src/api/routers/model.py
+++ fastapi/src/api/routers/model.py
@@ -37,16 +37,48 @@ async def photo_information(request: Request):
         "content": image_base64
     }, ensure_ascii=False))
 
-    logging.warning("added redis_connection", current_id)
     await redis_pubsub.subscribe(f"photo_response_{current_id}")
     start_time = time.time()
     while (time.time() - start_time) < 5:
         message = await redis_pubsub.get_message(ignore_subscribe_messages=True)
-        # if (random.randint(1, 30)) == 10:
-        await redis_connection.publish(f"photo_response_{current_id}", json.dumps({
-            "content": image_base64,
-            "time": time.time() - start_time
-        }, ensure_ascii=False))
+        if message is not None and message['data']:
+            return ApiResponse.payload({
+                'hello': message['data'].decode('utf-8')
+            })
+        await asyncio.sleep(0.1)
+
+    return ApiResponse.payload({
+        'detail': "Ошибка удаленного сервера"
+    })
+
+
+@router.post("/text", response_model=None)
+async def photo_information(request: Request):
+    try:
+        request_content = await request.json()
+        text = request_content['text']
+        logging.warning("got a text", text)
+        if len(text) > 500:
+            raise Exception()
+    except:
+        return ApiResponse.payload({
+            'error': True,
+            'message': 'Некорректный запрос'
+        })
+
+    redis_connection = redis.get_connection()
+    redis_pubsub = redis_connection.pubsub()
+
+    current_id = str(uuid.uuid4())
+    await redis_connection.publish("text_queries", json.dumps({
+        "id": current_id,
+        "content": text
+    }, ensure_ascii=False))
+
+    await redis_pubsub.subscribe(f"text_response_{current_id}")
+    start_time = time.time()
+    while (time.time() - start_time) < 5:
+        message = await redis_pubsub.get_message(ignore_subscribe_messages=True)
         if message is not None and message['data']:
             return ApiResponse.payload({
                 'hello': message['data'].decode('utf-8')
