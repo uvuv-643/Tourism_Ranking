edafcdf4866fd15e36615c6467ec158a5636493b
artem <uvuv.space@gmail.com>
2024-04-10 14:22:34 +0300

    added redis

--- fastapi/src/api/routers/model.py
+++ fastapi/src/api/routers/model.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter, Depends, Request
+import os
+
+import redis
+from fastapi import APIRouter
 
 from src.api.responses.api_response import ApiResponse
 
@@ -7,9 +10,17 @@ router = APIRouter(
     tags=["model"],
 )
 
+redis = redis.Redis(host='localhost', port=6379, db=0, password=os.getenv('REDIS_PASSWORD'))
+
 
 @router.post("/text", response_model=None)
 async def register():
+
+    if redis.get("h") is not None:
+        await redis.set("h", int(redis.get("h")) + 1)
+    else:
+        await redis.set("h", "0")
+
     return ApiResponse.payload({
-        'hello': 'world'
+        'hello': redis.get("h")
     })
