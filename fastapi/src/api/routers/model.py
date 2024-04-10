787a1fdeb6a727dc9d18874fbb0bfc1618d528a0
artem <uvuv.space@gmail.com>
2024-04-10 17:21:04 +0300

    fixed redis

--- fastapi/src/api/routers/model.py
+++ fastapi/src/api/routers/model.py
@@ -1,7 +1,11 @@
-import os
-
-from fastapi import APIRouter
+import asyncio
+import json
 import logging
+import random
+import time
+
+import uuid
+from fastapi import APIRouter, Request
 
 from src.api.responses.api_response import ApiResponse
 from src.utils.redis import redis
@@ -12,13 +16,37 @@ router = APIRouter(
 )
 
 
-@router.post("/text", response_model=None)
-async def register():
+@router.post("/photo", response_model=None)
+async def photo_information(request: Request):
+    try:
+        request_content = await request.json()
+        image_base64 = request_content['image'].split(',', 1)[1]
+    except:
+        return ApiResponse.payload({
+            'error': True,
+            'message': 'Некорректное base64 изображение'
+        })
+
     redis_connection = redis.get_connection()
-    if redis_connection.get("h") is not None:
-        redis_connection.set("h", str(int(redis_connection.get("h")) + 1))
-    else:
-        redis_connection.set("h", "0")
+    redis_pubsub = redis_connection.pubsub()
+
+    current_id = str(uuid.uuid4())
+    await redis_connection.publish("photo_queries", json.dumps({
+        "id": current_id,
+        "content": image_base64
+    }, ensure_ascii=False))
+
+    logging.warning("added redis_connection", current_id)
+    await redis_pubsub.subscribe(f"photo_response_{current_id}")
+    start_time = time.time()
+    while (time.time() - start_time) < 5:
+        message = redis_pubsub.get_message()
+        if message is not None:
+            return ApiResponse.payload({
+                'hello': message
+            })
+        await asyncio.sleep(0.1)
+
     return ApiResponse.payload({
-        'hello': str(redis_connection.get("h"))
+        'detail': "Ошибка удаленного сервера"
     })
