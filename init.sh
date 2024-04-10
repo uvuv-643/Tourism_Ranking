5349c477d0349bb898fb125c3858e7e72df91a41
artem <uvuv.space@gmail.com>
2024-04-10 20:41:13 +0000

    added init script

---
+++ init.sh
@@ -0,0 +1,10 @@
+sudo apt update -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
+sudo apt install docker-ce -y
+sudo usermod -aG docker dev
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+su - dev
+
