cf8fdbc106b6f0d055604b26398aede12531f04d
artem <uvuv.space@gmail.com>
2024-04-11 13:32:24 +0300

    added test routes

--- fastapi/src/api/routers/base.py
+++ fastapi/src/api/routers/base.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 
 from src.api.routers import model
+from src.api.routers import test
 
 
 def create_app_routers(app: FastAPI):
     app.include_router(model.router)
+    app.include_router(test.router)
     return app
